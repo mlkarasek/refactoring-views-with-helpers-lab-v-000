1fef23dd3775b3acbe3e537e34dbd4443c07309b
Monica  Karasek <mlkarasek@gmail.com>
2018-08-27 22:05:28 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  def display_artist
+    if @song.artist
+      link to @song.artist_name, artist_path(@song.artist)
+    else 
+      link_to @song.title, edit_song_path(@song)
+    end 
+  end 
 end
