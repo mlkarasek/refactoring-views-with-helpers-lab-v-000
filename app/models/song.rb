dc9f094b703fbc39a0458ba32db799438a0b80aa
Monica  Karasek <mlkarasek@gmail.com>
2018-08-27 22:13:43 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,11 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    artist.name if artist
+    self.artist.name
   end
 
   def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
